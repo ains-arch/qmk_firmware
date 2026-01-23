c2a6090a0ce57e80581f6743da3888cbdf8fa6f0
ains-arch <ainsleearchibald@gmail.com>
2026-01-23 11:47:44 -0700

    maximum RGB achieved

--- keyboards/crkbd/keymaps/ainsarch/keymap.c
+++ keyboards/crkbd/keymaps/ainsarch/keymap.c
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  //|   WIN  | 3rd L  |  ENTER |        | SPACE |  xxxx  | BSPACE  |
                                  //`--------------------------'        `--------------------------'
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-   KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,                        KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_ESC,
+   KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_P,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
 //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,
 //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //,-----------------------------------------------------.                    ,-----------------------------------------------------.
    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
 //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_VOLD, KC_PGDN, KC_PGUP, KC_VOLU, KC_PSCR, KC_F12,
+   KC_CAPS, BL_TOGG, BL_DOWN, BL_UP,   BL_BRTG,   KC_NO,                        KC_VOLD, KC_PGDN, KC_PGUP, KC_VOLU, KC_PSCR, KC_F12,
 //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+   KC_TRNS, RM_TOGG, RM_VALD, RM_VALU, RM_PREV, RM_NEXT,                      RM_HUED, RM_HUEU, RM_SATD, RM_SATU, RM_SPDD, RM_SPDU,
 //|--------+--------+--------+--------+--------+--------+-----.        .-----+--------+--------+--------+--------+--------+--------|
                                     KC_NO,   KC_TRNS,  KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS)
                                  //`--------------------------'        `--------------------------'
