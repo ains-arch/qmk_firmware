41b85def1352e6c3c0cfeb7b7bd211e65aa700ad
ains-arch <ainsleearchibald@gmail.com>
2026-02-10 22:24:49 -0700

    actually i don't need via

--- keyboards/crkbd/keymaps/ainsarch/rules.mk
+++ keyboards/crkbd/keymaps/ainsarch/rules.mk
@@ -5,10 +5,3 @@ RGB_MATRIX_ENABLE = yes
 # The Combo feature is a chording type solution for adding custom actions. It lets you hit multiple keys at once and produce a different effect.
 # COMBO_ENABLE = yes
 # COMBO_TERM = 30
-
-# This will cause the final step to take longer, but should get you a smaller compiled size.
-# This also disables Action Functions, and Action Macros, both of which are deprecated.
-LTO_ENABLE = yes
-
-# For editing with usevia.app
-VIA_ENABLE = yes
